805f8350b813286dd7e2635de1ea7e5f92cb5630
daidai-09 <daishiben94@gmail.com>
2025-10-09 16:05:36 +0900

    Update About.js
    About.jsの更新を行いました。

--- maiken-main/src/pages/About.js
+++ maiken-main/src/pages/About.js
@@ -3,15 +3,22 @@ import React from 'react';
 // MaikenIntro component
 const MaikenIntro = () => {
   return (
-    <div className="B_section">
-      <h2 className="B_title">まいけんとは</h2>
-      <ol className="B_ol">
-        <li className="B_li">舞鶴高専の鶴友寮の食堂専用アプリ</li>
-        <li className="B_li">自分の健康生活の評価</li>
-        <li className="B_li">料理への評価</li>
-        <li className="B_li">他の人からの料理への評価</li>
-        <li className="B_li">その日の献立の確認が出来ます。</li>
-      </ol>
+    <div>
+      <h2 >まいけんの説明</h2>
+      <p>
+        "まいけん" は鶴友寮の寮食専用アプリの事です。
+        <br></br>
+        食事へのコメントをしたり、他の人の食事の評価を確認することが出来ます。
+        <br></br>
+        このサイトを使用することで自らの食生活の評価や献立の確認が出来ます。
+      </p>
+      <h2>まいけんの機能</h2>
+      <ul>
+        <li type="sqare">食生活の評価の確認</li>
+        <li type="sqare">食事へのコメント</li>
+        <li type="sqare">食事へのコメントの閲覧</li>
+        <li type="sqare">その日の献立の確認</li>
+      </ul>
     </div>
   );
 };
@@ -19,10 +26,13 @@ const MaikenIntro = () => {
 // MaikenUsage component
 const MaikenUsage = () => {
   return (
-    <div className="A_section">
-      <h2 className="A_title">まいけんの使い方</h2>
-      <ol className="A_ol">
-        <li className="A_li">左上のボタンからサイドバーを開き、対象の位置に移動。</li>
+    <div >
+      <h2>まいけんの使い方</h2>
+      <ol>
+        <li>左上のボタンからサイドバーを開き、他のページに移動。</li>
+        <li>カレンダー機能では、選択した日の献立を閲覧することが出来ます。</li>
+        <li>自分の評価・レビューでは、料理への評価が出来ます。</li>
+        <li>コメント閲覧機能では、タイムライン形式で食事へのコメントが閲覧できます。</li>
       </ol>
     </div>
   );
@@ -30,8 +40,8 @@ const MaikenUsage = () => {
 
 const About = () => {
   return (
-    <div className="main-content">
-      <h2 className="page-title">説明・使い方</h2>
+    <div>
+      <h1>説明・使い方</h1>
       <MaikenIntro />
       <MaikenUsage />
     </div>
